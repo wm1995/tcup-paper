30c980ee855726b9e73eacae98b0670698aade54
William Martin <30499074+wm1995@users.noreply.github.com>
2022-12-16 14:44:51 +0000

    Add Python code to test model on datasets

---
+++ graphics/scripts/gen_data.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+import json
+import numpy as np
+import scipy.stats as sps
+
+# Set up run parameters
+SEED = 24601
+
+
+def write_dataset(name, data, params):
+    output = {
+        "data": data,
+        "params": params,
+    }
+    with open(f"data_{name}.json", "w") as f:
+        json.dump(output, f)
+
+
+def lin_rel(rng, x_true, alpha, beta, sigma_int):
+    (N, K) = x_true.shape
+
+    y_true = (
+        alpha
+        + np.dot(x_true, beta)
+        + sps.norm.rvs(0, sigma_int, size=(N,), random_state=rng)
+    )
+
+    return y_true
+
+
+def simple_x_obs(rng, x_true, err):
+    (N, K) = x_true.shape
+
+    dx = np.array([np.identity(K) for _ in range(N)]) * err
+    x_err = np.array(
+        [
+            sps.multivariate_normal.rvs(np.zeros(K), cov, random_state=rng)
+            for cov in dx
+        ]
+    ).reshape(N, K)
+    x = x_true + x_err
+
+    return x, dx
+
+
+def simple_y_obs(rng, y_true, err):
+    (N,) = y_true.shape
+
+    dy = np.ones(y_true.shape) * err
+    y = sps.norm.rvs(y_true, dy, random_state=rng)
+
+    return y, dy
+
+
+def no_outlier(rng, y_true):
+    return y_true, np.ones(y_true.shape, dtype=bool)
+
+
+def simple_outlier(rng, y_true, outlier):
+    (N,) = y_true.shape
+
+    # Set up outlier mask
+    mask = np.ones(N, dtype=bool)
+    mask[-2] = False
+
+    y_true[~mask] -= outlier
+
+    return y_true, mask
+
+
+def gen_data(
+    seed,
+    x_true,
+    y_true_fn,
+    y_true_params,
+    x_obs_fn,
+    x_obs_params,
+    y_obs_fn,
+    y_obs_params,
+    outlier_fn,
+    outlier_params,
+):
+    # Set up RNG
+    rng = np.random.default_rng(seed)
+
+    # Generate true y values
+    y_true = y_true_fn(rng, x_true, **y_true_params)
+
+    # Add outliers
+    y_true, outlier_mask = outlier_fn(rng, y_true, **outlier_params)
+
+    # Generate observed values
+    x, dx = x_obs_fn(rng, x_true, **x_obs_params)
+    y, dy = y_obs_fn(rng, y_true, **y_obs_params)
+
+    # Return data as dict
+    data = {
+        "x": x.tolist(),
+        "dx": dx.tolist(),
+        "y": y.tolist(),
+        "dy": dy.tolist(),
+    }
+    params = {
+        "y_true_params": y_true_params,
+        "outliers": outlier_mask.tolist(),
+        "x_obs_params": x_obs_params,
+        "y_obs_params": y_obs_params,
+    }
+    return data, params
+
+
+if __name__ == "__main__":
+    # Create datasets
+    # Dataset 1: linear relationship, dim x = 1, 0 outliers
+    data, params = gen_data(
+        seed=SEED,
+        x_true=np.linspace(0, 10, 12)[:, np.newaxis],
+        y_true_fn=lin_rel,
+        y_true_params={
+            "alpha": 3,
+            "beta": [2],
+            "sigma_int": 0.1,
+        },
+        x_obs_fn=simple_x_obs,
+        x_obs_params={"err": 0.2},
+        y_obs_fn=simple_y_obs,
+        y_obs_params={"err": 0.2},
+        outlier_fn=no_outlier,
+        outlier_params={},
+    )
+    write_dataset("linear_1D0", data, params)
+
+    # Dataset 2: linear relationship, dim x = 1, 1 outlier
+    data, params = gen_data(
+        seed=SEED,
+        x_true=np.linspace(0, 10, 12)[:, np.newaxis],
+        y_true_fn=lin_rel,
+        y_true_params={
+            "alpha": 3,
+            "beta": [2],
+            "sigma_int": 0.1,
+        },
+        x_obs_fn=simple_x_obs,
+        x_obs_params={"err": 0.2},
+        y_obs_fn=simple_y_obs,
+        y_obs_params={"err": 0.2},
+        outlier_fn=simple_outlier,
+        outlier_params={"outlier": 10},
+    )
+    write_dataset("linear_1D1", data, params)
+
+    # Dataset 3: linear relationship, dim x = 2, 0 outliers
+    data, params = gen_data(
+        seed=SEED,
+        x_true=np.random.default_rng(SEED).uniform(-1, 1, 200).reshape(-1, 2),
+        y_true_fn=lin_rel,
+        y_true_params={
+            "alpha": 3,
+            "beta": [2, 1],
+            "sigma_int": 0.1,
+        },
+        x_obs_fn=simple_x_obs,
+        x_obs_params={"err": 0.2},
+        y_obs_fn=simple_y_obs,
+        y_obs_params={"err": 0.2},
+        outlier_fn=no_outlier,
+        outlier_params={},
+    )
+    write_dataset("linear_2D0", data, params)
+
+    # Dataset 4: linear relationship, dim x = 2, 1 outlier
+    data, params = gen_data(
+        seed=SEED,
+        x_true=np.random.default_rng(SEED).uniform(-1, 1, 200).reshape(-1, 2),
+        y_true_fn=lin_rel,
+        y_true_params={
+            "alpha": 3,
+            "beta": [2, 1],
+            "sigma_int": 0.1,
+        },
+        x_obs_fn=simple_x_obs,
+        x_obs_params={"err": 0.2},
+        y_obs_fn=simple_y_obs,
+        y_obs_params={"err": 0.2},
+        outlier_fn=simple_outlier,
+        outlier_params={"outlier": 1.5},
+    )
+    write_dataset("linear_2D1", data, params)
+
+    # Dataset 5: linear relationship, dim x = 3, 0 outliers
+    data, params = gen_data(
+        seed=SEED,
+        x_true=np.random.default_rng(SEED).uniform(-1, 1, 90).reshape(-1, 3),
+        y_true_fn=lin_rel,
+        y_true_params={
+            "alpha": 3,
+            "beta": [2, 1, 3],
+            "sigma_int": 0.1,
+        },
+        x_obs_fn=simple_x_obs,
+        x_obs_params={"err": 0.2},
+        y_obs_fn=simple_y_obs,
+        y_obs_params={"err": 0.2},
+        outlier_fn=no_outlier,
+        outlier_params={},
+    )
+    write_dataset("linear_3D0", data, params)
+
+    # Dataset 6: linear relationship, dim x = 3, 1 outlier
+    data, params = gen_data(
+        seed=SEED,
+        x_true=np.random.default_rng(SEED).uniform(-1, 1, 90).reshape(-1, 3),
+        y_true_fn=lin_rel,
+        y_true_params={
+            "alpha": 3,
+            "beta": [2, 1, 3],
+            "sigma_int": 0.1,
+        },
+        x_obs_fn=simple_x_obs,
+        x_obs_params={"err": 0.2},
+        y_obs_fn=simple_y_obs,
+        y_obs_params={"err": 0.2},
+        outlier_fn=simple_outlier,
+        outlier_params={"outlier": 1.5},
+    )
+    write_dataset("linear_3D1", data, params)
---
+++ graphics/scripts/run_models.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+import json
+
+import arviz as az
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tcup.stan import tcup
+
+# Set up run parameters
+SEED = 24601
+
+
+def load_dataset(name):
+    with open(f"data_{name}.json", "r") as f:
+        dataset = json.load(f)
+
+    data = {key: np.array(val) for key, val in dataset["data"].items()}
+    params = dataset["params"]
+    params["outliers"] = np.array(params["outliers"])
+    return data, params
+
+
+if __name__ == "__main__":
+    datasets = [
+        "linear_1D0",
+        "linear_1D1",
+        "linear_2D0",
+        "linear_2D1",
+        "linear_3D0",
+        "linear_3D1",
+    ]
+
+    priors = [
+        "invgamma",
+        "cauchy",
+        "cauchy_scaled",
+        "cauchy_truncated",
+        "F18",
+        "F18reparam",
+        "nu2",
+        "nu2_principled",
+        "nu2_heuristic",
+        "nu2_scaled",
+        None,  # will be fixed to nu = 2
+    ]
+    models = [("tcup", prior) for prior in priors]
+    models.append(("ncup", None))
+
+    results = pd.DataFrame(
+        columns=[
+            "dataset",
+            "model",
+            "prior",
+            "r_hat",
+            "divergences",
+            "sigma_int",
+            "alpha",
+            "beta[0]",
+            "beta[1]",
+            "beta[2]",
+            "nu",
+            "peak_height",
+        ]
+    )
+
+    for dataset in datasets:
+        data, params = load_dataset(dataset)
+        for model, prior in models:
+            # Fit model
+            if model == "tcup" and prior is None:
+                fixed_nu = data.copy()
+                fixed_nu["nu"] = 2
+                mcmc = tcup(fixed_nu, SEED, model, prior)
+            else:
+                mcmc = tcup(data, SEED, model, prior)
+
+            # Save chains
+            mcmc.to_netcdf(f"{dataset}_{model}_{prior}.nc")
+
+            # Calculate summary
+            summary = az.summary(mcmc)
+
+            # Add results to table
+            results.loc[len(results)] = [
+                dataset,
+                model,
+                prior,
+                summary["r_hat"].max(),
+                np.sum(mcmc.sample_stats["diverging"].values),
+                summary["mean"].get("sigma", None),
+                summary["mean"].get("alpha", None),
+                summary["mean"].get("beta[0]", None),
+                summary["mean"].get("beta[1]", None),
+                summary["mean"].get("beta[2]", None),
+                summary["mean"].get("nu", None),
+                summary["mean"].get("peak_height", None),
+            ]
+
+            # Create trace plot
+            az.plot_trace(mcmc)
+            plt.savefig(f"trace_{dataset}_{model}_{prior}.pdf")
+            plt.close()
+
+            # Save results as we go
+            results.to_csv("results.csv", index=False)
+
+    results.to_csv("results.csv", index=False)

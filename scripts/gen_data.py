8c50f9721ac6e40ac2e430d562246e65537a6f7f
William Martin <30499074+wm1995@users.noreply.github.com>
2023-04-13 17:14:00 +0100

    Add simplified datasets script
    This isn't as flexible as the old one, but it's a lot less complicated
    and quite frankly does the job that it needs to.

--- scripts/gen_data.py
+++ scripts/gen_data.py
@@ -7,234 +7,166 @@ import scipy.stats as sps
 SEED = 24601
 
 
-def write_dataset(name, data, params):
+def write_dataset(name, data, info):
     output = {
         "data": data,
-        "params": params,
+        "info": info,
     }
     with open(f"data/{name}.json", "w") as f:
         json.dump(output, f)
 
 
-def t_dist(rng, x_true, alpha, beta, sigma_int, nu):
-    (N, K) = x_true.shape
+if __name__ == "__main__":
+    rng = np.random.default_rng(SEED)
 
-    y_true = (
-        alpha
-        + np.dot(x_true, beta)
-        + sps.t.rvs(nu, sigma_int, size=(N,), random_state=rng)
+    # Dataset 1
+    # t-distributed
+    shape = (20,)
+    info = {
+        "name": r"\textit{t}-distributed ($\nu = 3$)",
+        "alpha": 3,
+        "beta": 2,
+        "sigma_int": 0.1,
+        "nu": 3,
+    }
+    x_true = sps.norm(2, 2).rvs(size=shape, random_state=rng)
+    epsilon = sps.t(info["nu"], scale=info["sigma_int"]).rvs(
+        size=shape[0], random_state=rng
     )
-
-    return y_true
-
-
-def lin_rel(rng, x_true, alpha, beta, sigma_int):
-    (N, K) = x_true.shape
-
-    y_true = (
-        alpha
-        + np.dot(x_true, beta)
-        + sps.norm.rvs(0, sigma_int, size=(N,), random_state=rng)
-    )
-
-    return y_true
-
-
-def simple_x_obs(rng, x_true, err):
-    (N, K) = x_true.shape
-
-    dx = np.array([np.identity(K) for _ in range(N)]) * err
-    x_err = np.array(
-        [
-            sps.multivariate_normal.rvs(np.zeros(K), cov, random_state=rng)
-            for cov in dx
-        ]
-    ).reshape(N, K)
-    x = x_true + x_err
-
-    return x, dx
-
-
-def simple_y_obs(rng, y_true, err):
-    (N,) = y_true.shape
-
-    dy = np.ones(y_true.shape) * err
-    y = sps.norm.rvs(y_true, dy, random_state=rng)
-
-    return y, dy
-
-
-def no_outlier(rng, y_true):
-    return y_true, np.ones(y_true.shape, dtype=bool)
-
-
-def simple_outlier(rng, y_true, outlier):
-    (N,) = y_true.shape
-
-    # Set up outlier mask
-    mask = np.ones(N, dtype=bool)
-    mask[-2] = False
-
-    y_true[~mask] -= outlier
-
-    return y_true, mask
-
-
-def normal_outlier(rng, y_true, sigma_outlier, outlier_frac):
-    (N,) = y_true.shape
-
-    N_outlier = rng.binomial(N, outlier_frac)
-
-    # Set up outlier mask
-    mask = np.ones(N, dtype=bool)
-    inds = rng.choice(np.arange(N), N_outlier, replace=False)
-    mask[inds] = False
-
-    y_true[~mask] += sps.norm.rvs(
-        0, sigma_outlier, size=(N_outlier,), random_state=rng
-    )
-
-    return y_true, mask
-
-
-def gen_data(
-    seed,
-    x_true,
-    y_true_fn,
-    y_true_params,
-    x_obs_fn,
-    x_obs_params,
-    y_obs_fn,
-    y_obs_params,
-    outlier_fn,
-    outlier_params,
-):
-    # Set up RNG
-    rng = np.random.default_rng(seed)
-
-    # Generate true y values
-    y_true = y_true_fn(rng, x_true, **y_true_params)
-
-    # Add outliers
-    y_true, outlier_mask = outlier_fn(rng, y_true, **outlier_params)
-
-    # Generate observed values
-    x, dx = x_obs_fn(rng, x_true, **x_obs_params)
-    y, dy = y_obs_fn(rng, y_true, **y_obs_params)
-
-    # Return data as dict
+    y_true = info["alpha"] + np.dot(x_true, info["beta"]) + epsilon
+    dx = 10 ** sps.norm(-1, 0.1).rvs(size=shape, random_state=rng)
+    dy = 10 ** sps.norm(-0.7, 0.1).rvs(size=shape[0], random_state=rng)
+    x = sps.t(info["nu"], loc=x_true, scale=dx).rvs(random_state=rng)
+    y = sps.t(info["nu"], loc=y_true, scale=dy).rvs(random_state=rng)
     data = {
         "x": x.tolist(),
-        "dx": dx.tolist(),
         "y": y.tolist(),
+        "dx": dx.tolist(),
         "dy": dy.tolist(),
     }
-    params = {
-        "y_true_params": y_true_params,
-        "outliers": (~outlier_mask).tolist(),
-        "x_obs_params": x_obs_params,
-        "y_obs_params": y_obs_params,
+    write_dataset("t", data, info)
+
+    # Dataset 2
+    # Normally distributed with single outlier
+    shape = (12,)
+    info = {
+        "name": r"Normally distributed with outlier",
+        "alpha": 3,
+        "beta": 2,
+        "sigma_int": 0.1,
+        "outlier_idx": 10,
     }
-    return data, params
-
-
-if __name__ == "__main__":
-    # Create datasets
-    # Dataset 1: linear relationship, dim x = 1, 1 outlier
-    data, params = gen_data(
-        seed=SEED,
-        x_true=np.linspace(0, 10, 12)[:, np.newaxis],
-        y_true_fn=lin_rel,
-        y_true_params={
-            "alpha": 3,
-            "beta": [2],
-            "sigma_int": 0.1,
-        },
-        x_obs_fn=simple_x_obs,
-        x_obs_params={"err": 0.2},
-        y_obs_fn=simple_y_obs,
-        y_obs_params={"err": 0.2},
-        outlier_fn=simple_outlier,
-        outlier_params={"outlier": 10},
+    x_true = sps.norm(-3, 0.5).rvs(size=shape, random_state=rng)
+    epsilon = sps.norm(scale=info["sigma_int"]).rvs(
+        size=shape[0], random_state=rng
     )
-    write_dataset("linear_1D", data, params)
+    y_true = info["alpha"] + np.dot(x_true, info["beta"]) + epsilon
+    y_true[info["outlier_idx"]] -= 10
+    dx = 10 ** sps.norm(-1, 0.1).rvs(size=shape, random_state=rng)
+    dy = 10 ** sps.norm(-1, 0.1).rvs(size=shape[0], random_state=rng)
+    x = sps.norm(loc=x_true, scale=dx).rvs(random_state=rng)
+    y = sps.norm(loc=y_true, scale=dy).rvs(random_state=rng)
+    data = {
+        "x": x.tolist(),
+        "y": y.tolist(),
+        "dx": dx.tolist(),
+        "dy": dy.tolist(),
+    }
+    write_dataset("normal", data, info)
 
-    # Dataset 2: linear relationship, dim x = 2, 1 outlier
-    data, params = gen_data(
-        seed=SEED,
-        x_true=np.random.default_rng(SEED).uniform(-1, 1, 200).reshape(-1, 2),
-        y_true_fn=lin_rel,
-        y_true_params={
-            "alpha": 3,
-            "beta": [2, 1],
-            "sigma_int": 0.1,
-        },
-        x_obs_fn=simple_x_obs,
-        x_obs_params={"err": 0.2},
-        y_obs_fn=simple_y_obs,
-        y_obs_params={"err": 0.2},
-        outlier_fn=simple_outlier,
-        outlier_params={"outlier": 1.5},
+    # Dataset 3
+    # Spike-and-slab-type Gaussian mixture
+    shape = (200, 2)
+    info = {
+        "name": r"Gaussian mixture",
+        "alpha": 2,
+        "beta": [3, -1],
+        "sigma_int": 0.4,
+        "outlier_frac": 0.1,
+    }
+    outlier_mask = (
+        sps.bernoulli(info["outlier_frac"])
+        .rvs(size=shape[0], random_state=rng)
+        .astype(bool)
     )
-    write_dataset("linear_2D", data, params)
+    info["outlier_mask"] = outlier_mask.tolist()
 
-    # Dataset 3: linear relationship, dim x = 3, 1 outlier
-    data, params = gen_data(
-        seed=SEED,
-        x_true=np.random.default_rng(SEED).uniform(-1, 1, 90).reshape(-1, 3),
-        y_true_fn=lin_rel,
-        y_true_params={
-            "alpha": 3,
-            "beta": [2, 1, 3],
-            "sigma_int": 0.1,
-        },
-        x_obs_fn=simple_x_obs,
-        x_obs_params={"err": 0.2},
-        y_obs_fn=simple_y_obs,
-        y_obs_params={"err": 0.2},
-        outlier_fn=simple_outlier,
-        outlier_params={"outlier": 1.5},
+    x_true = np.concatenate(
+        [
+            sps.multivariate_normal.rvs([-3, 2], [[0.5, -1], [-1, 4]], 140),
+            sps.multivariate_normal.rvs([-1, -1], [[1, 0.2], [0.2, 0.8]], 60),
+        ]
     )
-    write_dataset("linear_3D", data, params)
+    int_scatter = sps.norm(scale=info["sigma_int"]).rvs(
+        size=shape[0], random_state=rng
+    )
+    epsilon = np.where(outlier_mask, 10 * int_scatter, int_scatter)
+    y_true = info["alpha"] + np.dot(x_true, info["beta"]) + epsilon
+    cov_x = sps.wishart.rvs(3, np.diag([0.1, 0.1]), size=shape[0])
+    eps_x = np.array(
+        [sps.multivariate_normal.rvs([0, 0], cov) for cov in cov_x]
+    )
+    x = x_true + eps_x
+    dy = 10 ** sps.norm(-1, 0.1).rvs(size=shape[0], random_state=rng)
+    data = {
+        "x": x.tolist(),
+        "y": y.tolist(),
+        "cov_x": cov_x.tolist(),
+        "dy": dy.tolist(),
+    }
+    write_dataset("gaussian_mix", data, info)
 
-    # Dataset 4: t-distribution data, dim x = 1, 1 outlier
-    data, params = gen_data(
-        seed=SEED,
-        x_true=np.linspace(-5, 5, 40)[:, np.newaxis],
-        y_true_fn=t_dist,
-        y_true_params={
-            "alpha": -1,
-            "beta": [0.5],
-            "sigma_int": 0.02,
-            "nu": 4,
-        },
-        x_obs_fn=simple_x_obs,
-        x_obs_params={"err": 0.1},
-        y_obs_fn=simple_y_obs,
-        y_obs_params={"err": 0.1},
-        outlier_fn=no_outlier,
-        outlier_params={},
+    # Dataset 4
+    # Laplace distributed
+    shape = (25,)
+    info = {
+        "name": r"Laplace distributed",
+        "alpha": -1,
+        "beta": 0.8,
+        "sigma_int": 0.2,
+    }
+    x_true = sps.uniform(-5, 5).rvs(size=shape, random_state=rng)
+    epsilon = sps.laplace(scale=info["sigma_int"]).rvs(
+        size=shape[0], random_state=rng
     )
-    write_dataset("t_dist_1D", data, params)
+    y_true = info["alpha"] + np.dot(x_true, info["beta"]) + epsilon
+    dx = 10 ** sps.norm(-1, 0.1).rvs(size=shape, random_state=rng)
+    dy = 10 ** sps.norm(-1, 0.1).rvs(size=shape[0], random_state=rng)
+    x = sps.norm(loc=x_true, scale=dx).rvs(random_state=rng)
+    y = sps.norm(loc=y_true, scale=dy).rvs(random_state=rng)
+    data = {
+        "x": x.tolist(),
+        "y": y.tolist(),
+        "dx": dx.tolist(),
+        "dy": dy.tolist(),
+    }
+    write_dataset("laplace", data, info)
 
-    # Dataset 5: Gaussian mixture, dim x = 1
-    data, params = gen_data(
-        seed=SEED,
-        x_true=np.linspace(-3, 6, 100)[:, np.newaxis],
-        y_true_fn=lin_rel,
-        y_true_params={
-            "alpha": -1,
-            "beta": [0.5],
-            "sigma_int": 0.02,
-        },
-        x_obs_fn=simple_x_obs,
-        x_obs_params={"err": 0.1},
-        y_obs_fn=simple_y_obs,
-        y_obs_params={"err": 0.1},
-        outlier_fn=normal_outlier,
-        outlier_params={
-            "sigma_outlier": 0.2,
-            "outlier_frac": 0.15,
-        },
+    # Dataset 5
+    # Lognormal
+    shape = (25,)
+    info = {
+        "name": r"Lognormally distributed",
+        "alpha": 4,
+        "beta": 8,
+        "sigma_int": 0.2,
+    }
+    x_true = sps.uniform(-5, 5).rvs(size=shape, random_state=rng)
+    epsilon = 10 ** sps.norm(loc=np.log10(info["sigma_int"]), scale=0.1).rvs(
+        size=shape[0], random_state=rng
     )
-    write_dataset("gaussian_mixture_1D", data, params)
+    epsilon = sps.laplace(scale=info["sigma_int"]).rvs(
+        size=shape[0], random_state=rng
+    )
+    y_true = info["alpha"] + np.dot(x_true, info["beta"]) + epsilon
+    dx = 10 ** sps.norm(-1, 0.1).rvs(size=shape, random_state=rng)
+    dy = 10 ** sps.norm(-1, 0.1).rvs(size=shape[0], random_state=rng)
+    x = sps.norm(loc=x_true, scale=dx).rvs(random_state=rng)
+    y = sps.norm(loc=y_true, scale=dy).rvs(random_state=rng)
+    data = {
+        "x": x.tolist(),
+        "y": y.tolist(),
+        "dx": dx.tolist(),
+        "dy": dy.tolist(),
+    }
+    write_dataset("lognormal", data, info)

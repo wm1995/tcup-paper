7eddf46d33b5c5cb957ec76f956a9d0599a24cfe
William Martin <30499074+wm1995@users.noreply.github.com>
2023-04-22 16:58:11 +0100

    Update summarise_mcmc to fit changes in gen_data

--- scripts/summarise_mcmc.py
+++ scripts/summarise_mcmc.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     results = pd.DataFrame(
         columns=[
             "dataset",
-            "prior",
+            "model",
             "r_hat",
             "divergences",
             "sigma_int",
@@ -17,32 +17,30 @@ if __name__ == "__main__":
             "beta[1]",
             "beta[2]",
             "nu",
-            "peak_height",
         ]
     )
 
     for filepath in Path("results/").glob("*.nc"):
         # Trim extension from filename
         filename = filepath.name[:-3]
-        dataset = "".join(filename.split("_")[:2])
-        prior = "".join(filename.split("_")[2:])
+        dataset = "".join(filename.split("_")[:-1])
+        model = "".join(filename.split("_")[-1])
         mcmc = az.from_netcdf(filepath)
         summary = az.summary(mcmc)
 
         # Add results to table
         results.loc[len(results)] = [
             dataset,
-            prior,
+            model,
             summary["r_hat"].max(),
             np.sum(mcmc.sample_stats["diverging"].values),
             summary["mean"].get("sigma", None),
-            summary["mean"].get("alpha", None),
-            summary["mean"].get("beta[0]", None),
-            summary["mean"].get("beta[1]", None),
-            summary["mean"].get("beta[2]", None),
+            summary["mean"].get("alpha_rescaled", None),
+            summary["mean"].get("beta_rescaled[0]", None),
+            summary["mean"].get("beta_rescaled[1]", None),
+            summary["mean"].get("beta_rescaled[2]", None),
             summary["mean"].get("nu", None),
-            summary["mean"].get("peak_height", None),
         ]
 
-    results.sort_values(["dataset", "prior"], inplace=True)
+    results.sort_values(["dataset", "model"], inplace=True)
     results.to_csv("results/results.csv", index=False)

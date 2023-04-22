e522c9e9917212d8c2d13ca8edda302f7a53c939
William Martin <30499074+wm1995@users.noreply.github.com>
2023-04-22 17:00:11 +0100

    Update normal dataset to mimic LSA version
    The demo I used in my LSA was quite good and convincing - I've tried to
    change the dataset to be similar to that version.

--- scripts/gen_data.py
+++ scripts/gen_data.py
@@ -53,17 +53,17 @@ if __name__ == "__main__":
         "name": r"Normally distributed with outlier",
         "alpha": 3,
         "beta": 2,
-        "sigma_int": 0.1,
+        "sigma_int": 0.2,
         "outlier_idx": 10,
     }
-    x_true = sps.norm(-3, 0.5).rvs(size=shape, random_state=rng)
+    x_true = np.sort(sps.norm(5, 3).rvs(size=shape, random_state=rng))
     epsilon = sps.norm(scale=info["sigma_int"]).rvs(
         size=shape[0], random_state=rng
     )
     y_true = info["alpha"] + np.dot(x_true, info["beta"]) + epsilon
     y_true[info["outlier_idx"]] -= 10
-    dx = 10 ** sps.norm(-1, 0.1).rvs(size=shape, random_state=rng)
-    dy = 10 ** sps.norm(-1, 0.1).rvs(size=shape[0], random_state=rng)
+    dx = 10 ** sps.norm(-0.5, 0.1).rvs(size=shape, random_state=rng)
+    dy = 10 ** sps.norm(-0.3, 0.1).rvs(size=shape[0], random_state=rng)
     x = sps.norm(loc=x_true, scale=dx).rvs(random_state=rng)
     y = sps.norm(loc=y_true, scale=dy).rvs(random_state=rng)
     data = {

717d0e4c3fcede0d18b22e7958ee13e2b1b18d7c
William Martin <30499074+wm1995@users.noreply.github.com>
2023-07-17 16:19:27 +0100

    Update calculation of outlier_frac and int scatter

--- tcup-paper/data/gaussian_mix.py
+++ tcup-paper/data/gaussian_mix.py
@@ -1,5 +1,32 @@
 import numpy as np
 import scipy.stats as sps
+from scipy.optimize import root
+
+
+def cdf(x, sigma_int, outlier_prob):
+    result = (1 - outlier_prob) * sps.norm.cdf(x, scale=sigma_int)
+    result += outlier_prob * sps.norm.cdf(x, scale=10 * sigma_int)
+    return result
+
+
+def sigma_68(sigma_int, outlier_prob):
+    # Define a function whose root gives sigma_68
+    def f(x):
+        # Take advantage of the CDF being symmetric
+        return cdf(x, sigma_int, outlier_prob) - sps.norm.cdf(1)
+
+    # Find the root
+    result = root(f, sigma_int)
+
+    # If the root was found successfully, return the answer
+    assert result.success
+    return result.x[0]
+
+
+def outlier_frac(outlier_prob):
+    # This is a symmetric distribution, so outlier fraction is this
+    # (no need to scale x and scale by sigma_int because it would cancel)
+    return 2 * cdf(-3, 1, outlier_prob)
 
 
 def gen_dataset(seed):
@@ -10,10 +37,13 @@ def gen_dataset(seed):
         "alpha": 2,
         "beta": [3, -1],
         "sigma_int": 0.4,
-        "outlier_frac": 0.1,
+        "outlier_probability": 0.1,
     }
+    info["sigma_68"] = sigma_68(info["sigma_int"], info["outlier_probability"])
+    info["outlier_frac"] = outlier_frac(info["outlier_probability"])
+
     outlier_mask = (
-        sps.bernoulli(info["outlier_frac"])
+        sps.bernoulli(info["outlier_probability"])
         .rvs(size=shape[0], random_state=rng)
         .astype(bool)
     )

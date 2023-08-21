089b9ae7df76e002a988733ede5fa283dad15f91
William Martin <30499074+wm1995@users.noreply.github.com>
2023-08-21 15:48:39 +0100

    Loop over seeds until r_hat < 1.1 in fit_model.py

--- scripts/fit_model.py
+++ scripts/fit_model.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import argparse
+import arviz as az
 from tcup import tcup
 from tcup_paper.data.io import load_dataset
-
-# Set up run parameters
-SEED = 24601
-N_SAMPLES = 5000
 
 
 if __name__ == "__main__":
@@ -14,6 +11,8 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument("-f", "--fixed", type=int)
     group.add_argument("-n", "--normal", action="store_true")
+    parser.add_argument("-s", "--num-samples", type=int, default=5000)
+    parser.add_argument("-r", "--random-seed", type=int)
     parser.add_argument("dataset")
     parser.add_argument("outfile")
     args = parser.parse_args()
@@ -21,14 +20,38 @@ if __name__ == "__main__":
     # Load dataset
     data, params = load_dataset(args.dataset)
 
-    # Fit model
-    if args.normal:
-        mcmc = tcup(data, SEED, model="ncup", num_samples=N_SAMPLES)
-    elif args.fixed:
-        data["nu"] = args.fixed
-        mcmc = tcup(data, SEED, num_samples=N_SAMPLES)
-    else:
-        mcmc = tcup(data, SEED, num_samples=N_SAMPLES)
+    if args.random_seed:
+        # Fit model
+        if args.normal:
+            mcmc = tcup(**data, seed=args.random_seed, model="ncup", num_samples=args.num_samples)
+        elif args.fixed:
+            mcmc = tcup(**data, seed=args.random_seed, model="fixed", shape_param=args.fixed, num_samples=args.num_samples)
+        else:
+            mcmc = tcup(**data, seed=args.random_seed, num_samples=args.num_samples)
 
-    # Save chains
-    mcmc.to_netcdf(args.outfile)
+        # Save chains
+        mcmc.to_netcdf(args.outfile)
+    else:
+        # No random seed provided - loop until r_hat < 1.1
+        # (Maximum of 20 loops)
+        for seed in range(20):
+            # Fit model
+            if args.normal:
+                mcmc = tcup(**data, seed=seed, model="ncup", num_samples=args.num_samples)
+            elif args.fixed:
+                mcmc = tcup(**data, seed=seed, model="fixed", shape_param=args.fixed, num_samples=args.num_samples)
+            else:
+                mcmc = tcup(**data, seed=seed, model="tcup", num_samples=args.num_samples)
+
+            sample_stats = az.summary(mcmc)
+
+            if sample_stats["r_hat"].max() < 1.1:
+                # Successful run - save output
+                mcmc.to_netcdf(args.outfile)
+                break
+            else:
+                # Failed run - save for diagnostic purposes
+                filename = args.outfile.split(".")[:-1].join("")
+                filename += f"_run_{seed}.nc"
+                mcmc.to_netcdf(filename)
+

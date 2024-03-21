31eb7e16de1d504d90ca6170db214c6b09f9c693
William Martin <30499074+wm1995@users.noreply.github.com>
2024-03-21 17:20:33 +0000

    Save SBC as full InferenceData

--- scripts/fit_sbc.py
+++ scripts/fit_sbc.py
@@ -8,6 +8,7 @@ import numpyro
 import numpyro.distributions as dist
 from tcup.numpyro import model_builder
 from tcup_paper.data.io import load_dataset
+import xarray
 
 MAX_SAMPLES = 200000
 SAFETY_MARGIN = 1.1
@@ -27,6 +28,28 @@ def mixture_prior(weights, means, vars):
                 covariance_matrix=vars,
             ),
         )
+
+
+def extend_inference_data(data, new_data):
+    new_groups = {}
+    # Loop over all InferenceData groups
+    for group in data.groups():
+        if group == "observed_data":
+            # This is just the dataset provided to the MCMC sampler
+            # So will be the same for both data and new_data
+            new_groups[group] = data[group]
+        else:
+            # Concatenate group on draw
+            new_groups[group] = xarray.concat(
+                [data[group], new_data[group]],
+                "draw",
+            )
+            # Reindex draw
+            new_groups[group]["draw"] = np.arange(
+                new_groups[group].sizes["draw"]
+            )
+
+    return az.InferenceData(**new_groups)
 
 
 if __name__ == "__main__":
@@ -60,32 +83,30 @@ if __name__ == "__main__":
     kernel = numpyro.infer.NUTS(tcup_model)
     mcmc = numpyro.infer.MCMC(kernel, num_chains=1, num_warmup=1000, num_samples=1023)
 
-    samples = None
+    results = None
     while True:
         mcmc.run(rng_key, x_scaled=data["x_scaled"], y_scaled=data["y_scaled"], cov_x_scaled=data["cov_x_scaled"], dy_scaled=data["dy_scaled"],)
-        new_samples = mcmc.get_samples(group_by_chain=True)
+        new_results = az.from_numpyro(mcmc)
 
-        if samples is None:
-            samples = new_samples
+        if results is None:
+            results = new_results
         else:
-            for key, value in new_samples.items():
-                samples[key] = np.concatenate([samples[key], value], axis=1)
+            results = extend_inference_data(results, new_results)
 
         min_ess = (
-            az.ess(samples, var_names=PARAMS_OF_INTEREST).to_array().min().item()
+            az.ess(results, var_names=PARAMS_OF_INTEREST).to_array().min().item()
         )
 
         if min_ess > 1023:
             break
 
-        curr_samples = samples["alpha_scaled"].shape[1]
+        curr_samples = results.posterior.sizes["draw"]
         required_samples = np.ceil(curr_samples * 1023 / min_ess).astype(int) - curr_samples
         print(min_ess, curr_samples, required_samples)
         mcmc.num_samples = np.clip(required_samples, 1000, 10000)
         mcmc.post_warmup_state = mcmc.last_state
         rng_key = mcmc.post_warmup_state.rng_key
 
-        mcmc_output = az.convert_to_inference_data(samples)
-        mcmc_output.to_netcdf(f"{args.outfile}.checkpoint")
+        results.to_netcdf(f"{args.outfile}.checkpoint")
 
-    mcmc_output.to_netcdf(args.outfile)
+    results.to_netcdf(args.outfile)

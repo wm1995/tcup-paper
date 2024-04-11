5b356e1c9e76aa3932470b90d1b978b5eb9a52e1
William Martin <30499074+wm1995@users.noreply.github.com>
2024-04-11 17:37:53 +0100

    Remove docker run shell script
    This has been superceded by the Makefile

--- docker_run.sh
+++
@@ -1,1 +0,0 @@
-docker run --mount type=bind,source=$(pwd)/data,target=/mcmc/data --mount type=bind,source=$(pwd)/results,target=/mcmc/results linmix --quiet -r 0 data/kelly.json results/linmix-kelly.json

35a2191997fa43a4d9768a51f90c5da784cd3348
William Martin <30499074+wm1995@users.noreply.github.com>
2024-07-11 16:39:59 +0100

    Update fixed t dataset to use normal obs

--- tcup-paper/data/fixed/t.py
+++ tcup-paper/data/fixed/t.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.stats as sps
+from tcup.utils import sigma_68
 
 
 def gen_dataset(seed):
@@ -9,17 +10,16 @@ def gen_dataset(seed):
         "name": r"\textit{t}-distributed ($\nu = 3$)",
         "alpha": 3,
         "beta": 2,
-        "sigma_int": 0.1,
+        "sigma_68": 0.1,
         "nu": 3,
     }
+    t_scale = info["sigma_68"] / sigma_68(info["nu"])
     x_true = sps.norm(2, 2).rvs(size=shape, random_state=rng)
-    epsilon = sps.t(info["nu"], scale=info["sigma_int"]).rvs(
-        size=shape[0], random_state=rng
-    )
+    epsilon = sps.t(info["nu"], scale=t_scale).rvs(size=shape[0], random_state=rng)
     y_true = info["alpha"] + np.dot(x_true, info["beta"]) + epsilon
     dx = 10 ** sps.norm(-1, 0.1).rvs(size=shape, random_state=rng)
     dy = 10 ** sps.norm(-0.7, 0.1).rvs(size=shape[0], random_state=rng)
-    x = sps.t(info["nu"], loc=x_true, scale=dx).rvs(random_state=rng)
-    y = sps.t(info["nu"], loc=y_true, scale=dy).rvs(random_state=rng)
+    x = sps.norm(loc=x_true, scale=dx).rvs(random_state=rng)
+    y = sps.norm(loc=y_true, scale=dy).rvs(random_state=rng)
 
     return x, y, dx, dy, info

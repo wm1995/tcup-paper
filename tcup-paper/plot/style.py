499f6524f561a22c1e475318c27234bf25f781c3
William Martin <30499074+wm1995@users.noreply.github.com>
2024-09-13 10:53:10 +0100

    Update paper plot style

--- tcup-paper/plot/style.py
+++ tcup-paper/plot/style.py
@@ -3,14 +3,13 @@ import matplotlib as mpl
 
 def apply_matplotlib_style():
     preamble = r"""
-    \usepackage{unicode-math}
-    \setmainfont{XITS-Regular.otf}
-    \setmathfont{XITSMath-Regular.otf}
+    \usepackage{newtxtext, newtxmath}
     """
     mpl.rcParams["text.usetex"] = True
+    mpl.rcParams["backend"] = "pgf"
     mpl.rcParams["pgf.preamble"] = preamble
     mpl.rcParams["pgf.rcfonts"] = False
-    mpl.rcParams["font.size"] = 12
+    mpl.rcParams["font.size"] = 9
     mpl.rcParams["font.family"] = "serif"
     mpl.rcParams["xtick.direction"] = "in"
     mpl.rcParams["ytick.direction"] = "in"

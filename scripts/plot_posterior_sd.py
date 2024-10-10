7202df6007be312898ceb6eeda23943a5d961dbf
William Martin <30499074+wm1995@users.noreply.github.com>
2024-10-10 15:40:09 +0100

    Update Posterior SD plot

--- scripts/plot_posterior_sd.py
+++ scripts/plot_posterior_sd.py
@@ -1,20 +1,18 @@
-import jax
-import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
 import numpyro
 import numpyro.distributions as dist
 from numpyro.infer import MCMC, NUTS
 import scipy.stats as sps
+from scipy.integrate import simpson
 from tqdm import tqdm
 
 from tcup_paper.plot import style
 
-nus = jnp.logspace(-0.5, 2.5, 100)
-num_datapoints = [100] # [10, 30, 100, 300]
+nus = np.logspace(-0.5, 2.5, 100)
+num_datapoints = [10, 30, 100, 300]
 
 rng = np.random.default_rng(0)
-rng_key = jax.random.PRNGKey(0)
 
 def norm_model(data):
     mu = numpyro.sample("mu", dist.Normal(0.0, 3.0))
@@ -28,43 +26,38 @@ def t_model(nu, data):
 
 if __name__ == "__main__":
     style.apply_matplotlib_style()
-
     posterior_std_ratio = np.zeros((len(num_datapoints), len(nus)))
     for idx, n in enumerate(num_datapoints):
         print(f"For {n=}")
+        xgrid = np.linspace(-10, 10, 10000) / np.sqrt(n)
+        post = np.ones((2, nus.shape[0], xgrid.shape[0]))
         for idy, nu in enumerate(tqdm(nus)):
             # Generate dataset
             x = sps.norm.rvs(size=(n,), random_state=rng)
 
-            # Run normal model
-            rng_key, rng_key_ = jax.random.split(rng_key)
+            z = x[:, np.newaxis] - xgrid[np.newaxis, :]
 
-            kernel = NUTS(norm_model)
-            num_samples = 2000
-            mcmc = MCMC(kernel, num_warmup=1000, num_samples=num_samples, progress_bar=False)
-            mcmc.run(
-                rng_key_, data=x
-            )
-            samples_norm = mcmc.get_samples()
+            prior = np.ones(xgrid.shape) # flat prior
+            prior = sps.norm.pdf(xgrid, scale=3)
 
-            # Run t model
-            rng_key, rng_key_ = jax.random.split(rng_key)
+            stdev = np.zeros(2)
+            for idz, dist in enumerate([sps.t(nu), sps.norm]):
+                ll = np.sum(dist.logpdf(z), axis=0)
+                post[idz, idy] = np.exp(ll) * prior
+                post[idz, idy] /= (xgrid[1] - xgrid[0]) * post[idz, idy].sum()
+                mean = np.average(xgrid, weights=post[idz, idy])
+                stdev[idz] = np.sqrt(np.average((xgrid - mean) ** 2, weights=post[idz, idy]))
 
-            kernel = NUTS(t_model)
-            num_samples = 2000
-            mcmc = MCMC(kernel, num_warmup=1000, num_samples=num_samples, progress_bar=False)
-            mcmc.run(
-                rng_key_, nu=nu, data=x
-            )
-            samples_t = mcmc.get_samples()
+            # Check we've covered the necessary range
+            assert np.isclose(simpson(post[idz, idy, :], x=xgrid), 1).all()
 
             # Calculate standard deviation ratio
-            posterior_std_ratio[idx, idy] = (samples_t["mu"].std() / samples_norm["mu"].std())
+            posterior_std_ratio[idx, idy] = stdev[0] / stdev[1]
 
     plt.figure(figsize=(10 / 3, 8 / 3))
-    plt.semilogx(nus, posterior_std_ratio.T, "+")
+    plt.semilogx(nus, posterior_std_ratio[2].T, "+")
     plt.xlim(0.25, 400)
-    plt.ylim(0.75, 1.5)
+    plt.ylim(0, 1.5)
     plt.axhline([1.0], color="k", linestyle="--")
     plt.xlabel(r"Shape parameter, $\nu$")
     plt.ylabel(r"Posterior standard deviation ratio")

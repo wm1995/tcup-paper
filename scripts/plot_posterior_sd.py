fd952e92b62262b4da056f250fd728230b5025a4
William Martin <30499074+wm1995@users.noreply.github.com>
2024-10-18 16:52:14 +0100

    Update posterior SD plot
    I've rewritten the code to use JAX and the GPU (I now have two versions,
    one that's vectorised and one that's not). I've also included the
    results of the Fisher approximation as a dashed line.

--- scripts/plot_posterior_sd.py
+++ scripts/plot_posterior_sd.py
@@ -1,64 +1,63 @@
+import jax
+import jax.numpy as jnp
+from jax.scipy.optimize import minimize
+import jax.scipy.stats as jsps
 import matplotlib.pyplot as plt
-import numpy as np
-import numpyro
-import numpyro.distributions as dist
-from numpyro.infer import MCMC, NUTS
-import scipy.stats as sps
-from scipy.integrate import simpson
+from matplotlib.ticker import ScalarFormatter
+from tensorflow_probability.substrates.jax.math import erfcx
 from tqdm import tqdm
 
 from tcup_paper.plot import style
 
-nus = np.logspace(-0.5, 2.5, 100)
-num_datapoints = [10, 30, 100, 300]
+jax.config.update("jax_enable_x64", True)
 
-rng = np.random.default_rng(0)
-
-def norm_model(data):
-    mu = numpyro.sample("mu", dist.Normal(0.0, 3.0))
-    sigma = 1
-    numpyro.sample("obs", dist.Normal(mu, sigma), obs=data)
-
-def t_model(nu, data):
-    mu = numpyro.sample("mu", dist.Normal(0.0, 3.0))
-    sigma = 1
-    numpyro.sample("obs", dist.StudentT(nu, mu, sigma), obs=data)
+@jax.jit
+def fisher_pred(nu):
+    f = jnp.sqrt(nu / 2)
+    q = (nu + 1) * (1 - jnp.sqrt(jnp.pi) * f * erfcx(f))
+    return 1 / jnp.sqrt(q)
 
 if __name__ == "__main__":
     style.apply_matplotlib_style()
-    posterior_std_ratio = np.zeros((len(num_datapoints), len(nus)))
-    for idx, n in enumerate(num_datapoints):
-        print(f"For {n=}")
-        xgrid = np.linspace(-10, 10, 10000) / np.sqrt(n)
-        post = np.ones((2, nus.shape[0], xgrid.shape[0]))
-        for idy, nu in enumerate(tqdm(nus)):
-            # Generate dataset
-            x = sps.norm.rvs(size=(n,), random_state=rng)
+    rng_key = jax.random.key(0)
+    dists = [jsps.t, jsps.norm]
+    nus = jnp.logspace(-1, 2.5, 100)
+    ratio = jnp.zeros(nus.shape)
+    xgrid = jnp.linspace(-2, 2, 3000)[:, jnp.newaxis]
+    num_datapoints = 100
+    post = jnp.ones((xgrid.shape[0], 2))
 
-            z = x[:, np.newaxis] - xgrid[np.newaxis, :]
+    for idx, nu in enumerate(tqdm(nus)):
+        rng_key, rng_key_ = jax.random.split(rng_key)
+        x = jax.random.normal(rng_key_, shape=(1, num_datapoints))
+        z = x - xgrid
 
-            prior = np.ones(xgrid.shape) # flat prior
-            prior = sps.norm.pdf(xgrid, scale=3)
+        prior = jnp.ones((xgrid.shape[0]))
 
-            stdev = np.zeros(2)
-            for idz, dist in enumerate([sps.t(nu), sps.norm]):
-                ll = np.sum(dist.logpdf(z), axis=0)
-                post[idz, idy] = np.exp(ll) * prior
-                post[idz, idy] /= (xgrid[1] - xgrid[0]) * post[idz, idy].sum()
-                mean = np.average(xgrid, weights=post[idz, idy])
-                stdev[idz] = np.sqrt(np.average((xgrid - mean) ** 2, weights=post[idz, idy]))
+        ll = jnp.sum(jsps.t.logpdf(z, nu), axis=1)
+        ll -= ll.max(axis=0)
 
-            # Check we've covered the necessary range
-            assert np.isclose(simpson(post[idz, idy, :], x=xgrid), 1).all()
+        ll_norm = jnp.sum(jsps.norm.logpdf(z), axis=1)
+        ll_norm -= ll_norm.max(axis=0)
 
-            # Calculate standard deviation ratio
-            posterior_std_ratio[idx, idy] = stdev[0] / stdev[1]
+        post = post.at[:, 0].set(jnp.exp(ll) * prior)
+        post = post.at[:, 1].set(jnp.exp(ll_norm) * prior)
+        post /= (xgrid[1] - xgrid[0]) * post.sum(axis=0)
+
+        mean = jnp.sum(xgrid * post, axis=0) / jnp.sum(post, axis=0)
+        stdev = jnp.sqrt(jnp.sum((xgrid - mean)**2 * post, axis=0)) / jnp.sum(post, axis=0)
+
+        ratio = ratio.at[idx].set(stdev[0] / stdev[1])
 
     plt.figure(figsize=(10 / 3, 8 / 3))
-    plt.semilogx(nus, posterior_std_ratio[2].T, "+")
-    plt.xlim(0.25, 400)
+    plt.semilogx(nus, ratio, "+")
+    plt.semilogx(nus, fisher_pred(nus), "k--")
+    plt.xlim(0.2, 300)
     plt.ylim(0, 1.5)
-    plt.axhline([1.0], color="k", linestyle="--")
+    plt.axhline([1.0], color="k", linestyle="--", alpha=0.5)
+    
+    ax = plt.gca()
+    ax.xaxis.set_major_formatter(ScalarFormatter())
     plt.xlabel(r"Shape parameter, $\nu$")
     plt.ylabel(r"Posterior standard deviation ratio")
     plt.tight_layout()

1758c38b82925d0f126932ef1e2ddd4ee22a6ff2
William Martin <30499074+wm1995@users.noreply.github.com>
2024-11-01 18:16:51 +0000

    Shrink default plotting font size

--- tcup-paper/plot/style.py
+++ tcup-paper/plot/style.py
@@ -9,7 +9,7 @@ def apply_matplotlib_style():
     mpl.rcParams["backend"] = "pgf"
     mpl.rcParams["pgf.preamble"] = preamble
     mpl.rcParams["pgf.rcfonts"] = False
-    mpl.rcParams["font.size"] = 9
+    mpl.rcParams["font.size"] = 8
     mpl.rcParams["font.family"] = "serif"
     mpl.rcParams["xtick.direction"] = "in"
     mpl.rcParams["ytick.direction"] = "in"

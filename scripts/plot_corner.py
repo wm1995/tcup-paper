76cbe9ab67899648f1be409179432fddf288771b
William Martin <30499074+wm1995@users.noreply.github.com>
2024-11-01 18:19:10 +0000

    Extend corner plots to set custom tickers

--- scripts/plot_corner.py
+++ scripts/plot_corner.py
@@ -2,6 +2,7 @@ import argparse
 
 import arviz as az
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
 import numpy as np
 from tcup_paper.data.io import load_dataset
 from tcup_paper.plot.corner import plot_corner
@@ -22,10 +23,18 @@ if __name__ == "__main__":
         required=False,
     )
     parser.add_argument(
+        "--ticker",
+        action="append",
+        nargs=3,
+        metavar=("var_name", "base", "offset"),
+        required=False,
+    )
+    parser.add_argument(
         "--var-names",
         nargs="+",
         required=False,
     )
+    parser.add_argument("--single-column", action="store_true")
     args = parser.parse_args()
 
     # Set style
@@ -39,7 +48,7 @@ if __name__ == "__main__":
         true_vals = {
             "alpha": params["alpha"],
             "beta": params["beta"],
-            "sigma_68": params.get("sigma_68", params["sigma_int"]),
+            "sigma_68": params.get("sigma_68", params.get("sigma_int")),
             "nu": params.get("nu"),
             "outlier_frac": params.get("outlier_frac"),
         }
@@ -58,7 +67,7 @@ if __name__ == "__main__":
             var_bins = np.linspace(float(lower), float(upper), 50)
             if var_name == "sigma":
                 bins["sigma_68"] = var_bins
-                bins["sigma_rescaled"] = var_bins
+                bins["sigma"] = var_bins
             elif var_name[:4] == "beta":
                 bins[var_name] = var_bins
             elif var_name == "alpha":
@@ -88,20 +97,34 @@ if __name__ == "__main__":
                 elif var_name == "outlier_frac":
                     if "tcup" in mcmc_file:
                         var_names.append("outlier_frac")
+                elif var_name == "sigma_int":
+                    var_names.append("sigma")
                 else:
                     raise ValueError("Unrecognised variable")
 
         if first_pass:
             first_vars = var_names
 
-        var_labels = {
-            "alpha": r"Intercept $\alpha$",
-            "beta": r"Gradient $\beta$",
-            "sigma_rescaled": r"Int. scatter $\sigma_{\rm int}$",
-            "sigma_68": r"Int. scatter $\sigma_{68}$",
-            "nu": r"Shape param. $\nu$",
-            "outlier_frac": r"Outlier frac. $\omega$",
-        }
+        if args.single_column:
+            var_labels = {
+                "alpha": r"$\alpha$",
+                "beta": r"$\beta$",
+                "sigma": r"$\sigma_{\rm int}$",
+                "sigma_68": r"$\sigma_{68}$",
+                "nu": r"$\nu$",
+                "outlier_frac": r"$\omega$",
+            }
+            subplot_kwargs = {"figsize": (10 / 3, 10 / 3)}
+        else:
+            var_labels = {
+                "alpha": r"Intercept $\alpha$",
+                "beta": r"Gradient $\beta$",
+                "sigma": r"Int. scatter $\sigma_{\rm int}$",
+                "sigma_68": r"Int. scatter $\sigma_{68}$",
+                "nu": r"Shape param. $\nu$",
+                "outlier_frac": r"Outlier frac. $\omega$",
+            }
+            subplot_kwargs = {"figsize": (7.06, 7.06)}
 
         # Set styles for different datasets
         if "/normal/" in mcmc_file:
@@ -135,8 +158,12 @@ if __name__ == "__main__":
                     key + "s": value for key, value in mcmc_kwargs.items()
                 },
             },
+            true_kwargs={
+                "alpha": 0.4,
+                "dashes": (3, 3),
+            },
             bins=bins,
-            subplot_kwargs={"figsize": (6.32, 6.32)},
+            subplot_kwargs=subplot_kwargs,
             fig=fig,
             ax=ax,
         )
@@ -154,12 +181,30 @@ if __name__ == "__main__":
         else:
             ax_vars += [var_name]
 
+    locators = {}
+    if args.ticker is not None:
+        for var_name, base, offset in args.ticker:
+            locators[var_name] = plticker.MultipleLocator(
+                base=float(base),
+                offset=float(offset),
+            )
+
     for subplot_ax, var_name in zip(ax[-1, :], ax_vars):
         curr_bins = bins[var_name]
         subplot_ax.set_xlim(curr_bins.min(), curr_bins.max())
+        if var_name in locators:
+            subplot_ax.xaxis.set_major_locator(locators[var_name])
 
     for subplot_ax, var_name in zip(ax[1:, 0], ax_vars[1:]):
         curr_bins = bins[var_name]
         subplot_ax.set_ylim(curr_bins.min(), curr_bins.max())
+        if var_name in locators:
+            subplot_ax.yaxis.set_major_locator(locators[var_name])
+            
+    if "park" in args.mcmc_file[0]:
+        ax[1, 0].set_ylabel(r"$\beta$")
+        ax[2, 0].set_ylabel(r"$\gamma$")
+        ax[-1, 1].set_xlabel(r"$\beta$")
+        ax[-1, 2].set_xlabel(r"$\gamma$")
 
     plt.savefig(args.output)
